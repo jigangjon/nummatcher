55ec6738e090938f66b7399670423d56133b831e
jigangjon <jigangjon@gmail.com>
2025-11-01 22:48:10 +0900

    formatted

--- app/utils/pratt-parser.ts
+++ app/utils/pratt-parser.ts
@@ -2,47 +2,18 @@
  * Math expression parser with Pratt's algorithm
  * https://web.archive.org/web/20150228044653/http://effbot.org/zone/simple-top-down-parsing.htm
  */
-var tokenIndex = 0;
-const testExpression1 =
-  "  - 3.1231  ^ - 1   . 2 +   +412.23   4  *2-1\n/+3 - 3.1231 +   +412.23   4  *2-1\n/+3 - 3.1231 +   +412.23   4  *2-1\n/+3 - 3.1231 +   +412.23   4  *2-1\n/+3";
-const testExpression2 = "4^2^3 ";
-const testExpression3 = "sq rt ((3+3) !!!)";
-const testExpression4 = "root(27,3) + P(5,2) - 5c2";
-const ALL_OPERATORS: Tokens = {
-  "+": () => operatorToken("+", 10, prefixNud(20), infixLed(10)),
-  "-": () => operatorToken("-", 10, prefixNud(20), infixLed(10)),
-  "*": () => operatorToken("*", 20, undefined, infixLed(20)),
-  "/": () => operatorToken("/", 20, undefined, infixLed(20)),
-  "^": () => operatorToken("^", 30, undefined, infixLed(29)), // right associative
-  "!": () => operatorToken("!", 50, undefined, suffixLed()),
-  sqrt: () => operatorToken("sqrt", 60, prefixNud(60)),
-  "!!": () => operatorToken("!!", 50, undefined, suffixLed()),
-  "(": () => operatorToken("(", 200, parenNud()),
-  ")": () => operatorToken(")"),
-  ",": () => operatorToken(","),
-  root: () => operatorToken("root", 0, functionNud(2)),
-  p: () => operatorToken("p", 40, functionNud(2), infixLed(40)),
-  c: () => operatorToken("c", 40, functionNud(2), infixLed(40)),
-};
-const tokenList = tokenize(testExpression4, ALL_OPERATORS);
-function expression(rbp: number): Token {
-  let currentIndex = tokenIndex;
-  tokenIndex = tokenIndex + 1;
-  if (!tokenList[currentIndex].nud) {
-    throw new Error(`Unexpected token: ${tokenList[currentIndex].symbol}`);
-  }
-  let left = tokenList[currentIndex].nud();
-  while (rbp < tokenList[tokenIndex].lbp) {
-    currentIndex = tokenIndex;
-    tokenIndex = currentIndex + 1;
-    if (!tokenList[currentIndex].led) {
-      throw new Error(`Unexpected token: ${tokenList[currentIndex].symbol}`);
-    }
-    left = tokenList[currentIndex].led(left);
-  }
-  return left;
+
+interface Token {
+  symbol: string;
+  value?: number | string;
+  lbp: number;
+  nud?(tokenList: Token[]): Token;
+  led?(left: Token, tokenList: Token[]): Token;
+  first?: Token;
+  second?: Token;
 }
-console.log(expression(0));
+
+type Tokens = Record<string, () => Token>;
 
 function tokenize(input: string, operators: Tokens): Token[] {
   input = input.toLowerCase().replace(/\s+/g, "");
@@ -91,14 +62,23 @@ function tokenize(input: string, operators: Tokens): Token[] {
   return tokens;
 }
 
-interface Token {
-  symbol: string;
-  value?: number | string;
-  lbp: number;
-  nud?(): Token;
-  led?(left: Token): Token;
-  first?: Token;
-  second?: Token;
+var tokenIndex = 0;
+function expression(rbp: number, tokenList: Token[]): Token {
+  let currentIndex = tokenIndex;
+  tokenIndex = tokenIndex + 1;
+  if (!tokenList[currentIndex].nud) {
+    throw new Error(`Unexpected token: ${tokenList[currentIndex].symbol}`);
+  }
+  let left = tokenList[currentIndex].nud(tokenList);
+  while (rbp < tokenList[tokenIndex].lbp) {
+    currentIndex = tokenIndex;
+    tokenIndex = currentIndex + 1;
+    if (!tokenList[currentIndex].led) {
+      throw new Error(`Unexpected token: ${tokenList[currentIndex].symbol}`);
+    }
+    left = tokenList[currentIndex].led(left, tokenList);
+  }
+  return left;
 }
 
 function numberToken(symbol: string): Token {
@@ -115,8 +95,8 @@ function numberToken(symbol: string): Token {
 function operatorToken(
   symbol: string,
   lbp: number = 0,
-  nud?: () => Token,
-  led?: (left: Token) => Token
+  nud?: (tokenList: Token[]) => Token,
+  led?: (left: Token, tokenList: Token[]) => Token
 ): Token {
   return {
     symbol,
@@ -127,24 +107,24 @@ function operatorToken(
 }
 
 function infixLed(bp: number) {
-  function led(this: Token, left: Token): Token {
+  function led(this: Token, left: Token, tokenList: Token[]): Token {
     this.first = left;
-    this.second = expression(bp);
+    this.second = expression(bp, tokenList);
     return this;
   }
   return led;
 }
 
 function prefixNud(bp: number) {
-  function nud(this: Token): Token {
-    this.first = expression(bp);
+  function nud(this: Token, tokenList: Token[]): Token {
+    this.first = expression(bp, tokenList);
     return this;
   }
   return nud;
 }
 
 function suffixLed() {
-  function led(this: Token, left: Token): Token {
+  function led(this: Token, left: Token, tokenList: Token[]): Token {
     this.first = left;
     return this;
   }
@@ -152,23 +132,23 @@ function suffixLed() {
 }
 
 function parenNud() {
-  function nud(this: Token): Token {
-    const expr = expression(0);
-    advance(")");
+  function nud(this: Token, tokenList: Token[]): Token {
+    const expr = expression(0, tokenList);
+    advance(")", tokenList);
     return expr;
   }
   return nud;
 }
 
 function functionNud(arity: 1 | 2) {
-  function nud(this: Token): Token {
-    advance("(");
-    this.first = expression(0);
+  function nud(this: Token, tokenList: Token[]): Token {
+    advance("(", tokenList);
+    this.first = expression(0, tokenList);
     if (arity === 2) {
-      advance(",");
-      this.second = expression(0);
+      advance(",", tokenList);
+      this.second = expression(0, tokenList);
     }
-    advance(")");
+    advance(")", tokenList);
     return this;
   }
   return nud;
@@ -181,11 +161,33 @@ function endToken(): Token {
   };
 }
 
-function advance(symbol: string) {
+function advance(symbol: string, tokenList: Token[]) {
   if (tokenList[tokenIndex].symbol !== symbol) {
     throw new Error(`Expected token: ${symbol}`);
   }
   tokenIndex++;
 }
 
-type Tokens = Record<string, () => Token>;
+const testExpression1 =
+  "  - 3.1231  ^ - 1   . 2 +   +412.23   4  *2-1\n/+3 - 3.1231 +   +412.23   4  *2-1\n/+3 - 3.1231 +   +412.23   4  *2-1\n/+3 - 3.1231 +   +412.23   4  *2-1\n/+3";
+const testExpression2 = "4^2^3 ";
+const testExpression3 = "sq rt ((3+3) !!!)";
+const testExpression4 = "root(27,3) + P(5,2) - 5c2";
+const ALL_OPERATORS: Tokens = {
+  "+": () => operatorToken("+", 10, prefixNud(20), infixLed(10)),
+  "-": () => operatorToken("-", 10, prefixNud(20), infixLed(10)),
+  "*": () => operatorToken("*", 20, undefined, infixLed(20)),
+  "/": () => operatorToken("/", 20, undefined, infixLed(20)),
+  "^": () => operatorToken("^", 30, undefined, infixLed(29)), // right associative
+  "!": () => operatorToken("!", 50, undefined, suffixLed()),
+  sqrt: () => operatorToken("sqrt", 60, prefixNud(60)),
+  "!!": () => operatorToken("!!", 50, undefined, suffixLed()),
+  "(": () => operatorToken("(", 200, parenNud()),
+  ")": () => operatorToken(")"),
+  ",": () => operatorToken(","),
+  root: () => operatorToken("root", 0, functionNud(2)),
+  p: () => operatorToken("p", 40, functionNud(2), infixLed(40)),
+  c: () => operatorToken("c", 40, functionNud(2), infixLed(40)),
+};
+const tokenList = tokenize(testExpression4, ALL_OPERATORS);
+console.log(expression(0, tokenList));
